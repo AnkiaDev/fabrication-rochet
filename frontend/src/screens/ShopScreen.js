bcfb17f8503b5a0d0637fadf081f4636a24990d0
Ankia <AnkiaDev@gmail.com>
2021-03-05 13:14:00 +0100

    Add mongoDB, error handler, and server router middleware

--- frontend/src/screens/ShopScreen.js
+++ frontend/src/screens/ShopScreen.js
@@ -14,40 +14,32 @@ const ShopScreen = () => {
     fetchProducts();
   }, []);
   // Creating Shop Category
-  const savonArray = [];
-  const accessoryArray = [];
+  const categoryList = [];
   for (const product of products) {
-    if (product.type === "Savon") {
-      savonArray.push(product);
-    } else if (product.type === "Accessoire") {
-      accessoryArray.push(product);
+    const thisCategory = categoryList.includes(product.category);
+    if (thisCategory) {
+      continue;
+    } else {
+      categoryList.push(product.category);
     }
   }
   // Component
   return (
     <ListGroup variant="flush">
-      {/* Savon Section */}
-      <ListGroup.Item>
-        <h3 className="mt-3">Savons</h3>
-        <Row>
-          {savonArray.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
-      </ListGroup.Item>
-      {/* Accessory Section */}
-      <ListGroup.Item>
-        <h3 className="mt-3">Accessoires</h3>
-        <Row>
-          {accessoryArray.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-        </Row>
-      </ListGroup.Item>
+      {categoryList.map((category) => (
+        <ListGroup.Item key={category}>
+          <h3 className="mt-3">{category}</h3>
+          <Row>
+            {products
+              .filter((product) => product.category === category)
+              .map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              ))}
+          </Row>
+        </ListGroup.Item>
+      ))}
     </ListGroup>
   );
 };
